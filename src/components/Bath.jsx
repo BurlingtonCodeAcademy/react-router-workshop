8c5048d18c929afa7b1b2413aa5539271e4ddc8b
limzkil <78449568+limzkil@users.noreply.github.com>
2021-11-04 18:16:19 -0400

    styling

--- src/components/Bath.jsx
+++ src/components/Bath.jsx
@@ -1,11 +1,18 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+
+import { styles } from "../styles/styles.js";
 
 export default function Bath() {
-    return(
-        <>
-          <h1>You find your wallet in your pants from yesterday!</h1>
-          {/* use Link in place of a href to take back to the main page */}
-          
-        </>
-        )
-      }
+  return (
+    <>
+      <div style = {styles.Theme}>
+        <h1 style={styles.Theme.header}>
+          You find your wallet in your pants from yesterday!
+        </h1>
+        {/* use Link in place of a href to take back to the main page */}
+        <Link to="/" style = {styles.Theme.buttons}>Back to front door</Link>
+      </div>
+    </>
+  );
+}
+
--- src/styles/styles.js
+++ src/styles/styles.js
@@ -1,42 +1,43 @@
 const Theme = {
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
-  fontFamily: "'Merriweather', serif",
-  h3: {
-    fontFamily: "'Work Sans', sans-serif",
-    fontWeight: "800",
-    fontSize: "1.5vw",
-    textAlign: "center",
-  },
-  buttons: {
-    flexDirection: "row",
-    textDecoration: "none",
-    fontFamily: "'Work Sans', sans-serif",
-    fontWeight: "700",
-    color: "black",
-    padding: ".5vw",
-    margin: "1vw",
-    outline: ".1vw solid black"
-    
-  },
-  submit: {
-    flexDirection: "row",
-    textDecoration: "none",
-    fontFamily: "'Work Sans', sans-serif",
-    fontWeight: "700",
-    color: "black",
-    padding: ".5vw",
-    margin: "1vw",
-
-  },
-  buttonBox: {
-    flexDirection: "row",
-    margin: "1vw"
-    
-  },
-};
-
-export const styles = {
-  Theme: Theme,
-};
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    fontFamily: "'Merriweather', serif",
+    header: {
+      fontFamily: "'Work Sans', sans-serif",
+      fontWeight: "800",
+      fontSize: "1.5vw",
+      textAlign: "center",
+    },
+    buttons: {
+      flexDirection: "row",
+      textDecoration: "none",
+      fontFamily: "'Work Sans', sans-serif",
+      fontWeight: "700",
+      color: "black",
+      padding: ".5vw",
+      margin: "1vw",
+      outline: ".1vw solid black"
+      
+    },
+    submit: {
+      flexDirection: "row",
+      textDecoration: "none",
+      fontFamily: "'Work Sans', sans-serif",
+      fontWeight: "700",
+      color: "black",
+      padding: ".5vw",
+      margin: "1vw",
+  
+    },
+    buttonBox: {
+      flexDirection: "row",
+      margin: "1vw"
+      
+    },
+  };
+  
+  export const styles = {
+    Theme: Theme,
+  };
+  
